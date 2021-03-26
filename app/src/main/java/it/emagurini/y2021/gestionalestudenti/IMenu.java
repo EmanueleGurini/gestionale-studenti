d7ba1a9e4f9c928b955b98d06094894a0f2ddda5
Emanuele Gurini <emanuele.gurini89@gmail.com>
2021-03-26 12:27:02 +0100

    Set interface

---
+++ app/src/main/java/it/emagurini/y2021/gestionalestudenti/IMenu.java
@@ -0,0 +1,14 @@
+package it.emagurini.y2021.gestionalestudenti;
+
+public interface IMenu {
+
+    IStudente aggiungiStudente( new IStudente(String nome, String cognome, int matricola ) );
+
+    IEsame aggiungiEsame( new IEsame(double voto) );
+
+    int stampaMedia();
+
+    IClasse elencoStudenti();
+
+
+}
